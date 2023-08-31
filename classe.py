ef3af4a0fc2b1324c824c6b6c7ba1328ab79327d
pablo11-dev <131197454+pablo11-dev@users.noreply.github.com>
2023-08-31 09:20:39 -0300

    Create classe.py

---
+++ classe.py
@@ -0,0 +1,17 @@
+class ToDoList:
+    def __init__(self, tarefa):
+        self.tarefa = tarefa
+
+        self.adctarefa = {}
+
+    def adicionar_tarefa(self, descricao):
+
+        self.descricao = descricao
+        
+    def excluir_tarefa(self,):
+        pass
+
+    def listar_tarefas():
+        
+    
+        pass
